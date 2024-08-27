c105cd50f97e032a52120fb42ed4002af15cc827
Leo42night <109854127+Leo42night@users.noreply.github.com>
2024-08-28 05:35:19 +0700

    bersihkan dan rapikan

--- ambil.php
+++ ambil.php
@@ -7,7 +7,7 @@ try {
   $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // Query untuk mengambil data PoI
+  // Query untuk mengambil data PoI untuk megnhitung jarak
   $stmt = $pdo->query('SELECT name, lat, lng FROM poi');
   $poiData = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
